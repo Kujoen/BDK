38d9366647ff3a706db770eb0d73a4fee4ddc0b3
Andreas Farley <st141408@stud.uni-stuttgart.de>
2017-11-13 14:15:04 +0100

    Added property listener to actor, next up is changing the notifyDataChanged structure of the bdkActorEditor so panels no longer call notifyDataChanged on the main panel

--- src/bdk/game/entities/sprites/actors/Actor.java
+++ src/bdk/game/entities/sprites/actors/Actor.java
@@ -1,10 +1,13 @@
 package bdk.game.entities.sprites.actors;
 
 import java.awt.Graphics2D;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 import java.util.ArrayList;
 import java.util.List;
 
 import bdk.game.entities.sprites.Sprite;
+import bdk.game.entities.sprites.actors.components.emitter.EmitOnce;
 import bdk.game.entities.sprites.actors.components.emitter.Emitter;
 import bdk.game.entities.sprites.actors.components.initializers.Initializer;
 import bdk.game.entities.sprites.actors.components.operators.Operator;
@@ -14,60 +17,94 @@ import bdk.game.entities.sprites.actors.components.operators.Operator;
  * @author Andreas Farley
  *
  */
-public class Actor extends Sprite{
+public class Actor extends Sprite {
 
-	//--Properties
-	private String actorType;
-	private String actorName;
-	private int health;
-	//--Finals
+	// --Finals
 	public static final String ACTOR_ENEMY = "enemy";
 	public static final String ACTOR_PLAYER = "player";
 	public static final String ACTOR_PROJECTILE = "projectile";
-	public static final String[] ACTOR_TYPES = new String[]{ACTOR_ENEMY, ACTOR_PLAYER, ACTOR_PROJECTILE};
-	//--Controllers
+	public static final String[] ACTOR_TYPES = new String[] { ACTOR_ENEMY, ACTOR_PLAYER, ACTOR_PROJECTILE };
+	// --Properties
+	private String actorType;
+	private String actorName;
+	private int health;
+	// --Controllers
 	private Emitter emitter;
 	private List<Initializer> initializerList;
 	private List<Operator> operatorList;
 	private List<Actor> childList;
-	
+
 	public Actor(String actorName) {
 		super(0, 0, Sprite.TYPE_ACTOR);
 		this.actorName = actorName;
+		this.initializerList = new ArrayList<>();
+		this.operatorList = new ArrayList<>();
+		this.childList = new ArrayList<>();
 	}
+
+	// ---------------------------------------------------------------------|
 
 	@Override
 	public void update() {
 		// TODO Auto-generated method stub
 	}
-	
+
 	@Override
 	public void render(Graphics2D g) {
 		// TODO Auto-generated method stub
 	}
 
-	//---------------------------------------------------------------------|
-	//GETTERS & SETTERS
-	//---------------------------------------------------------------------|
-	
+	// LISTENER STUFF-------------------------------------------------------|
+
+	private final List<PropertyChangeListener> listeners = new ArrayList<>();
+
+	public void addPropertyChangeListener(PropertyChangeListener listener) {
+		listeners.add(listener);
+	}
+
+	private void firePropertyChange(String property, Object oldValue, Object newValue) {
+		for (PropertyChangeListener l : listeners) {
+			l.propertyChange(new PropertyChangeEvent(this, property, oldValue, newValue));
+		}
+	}
+	// TESTING LISTENER-----------------------------------------------------|
+
+	public static void main(String[] args) {
+
+		Actor testActor = new Actor("test");
+		testActor.addPropertyChangeListener(new PropertyChangeListener() {
+			@Override
+			public void propertyChange(PropertyChangeEvent arg0) {
+				System.out.println(arg0.getPropertyName() + " changed");
+			}
+		});
+
+		testActor.setEmitter(new EmitOnce());
+
+	}
+
+	// ---------------------------------------------------------------------|
+	// GETTERS & SETTERS
+	// ---------------------------------------------------------------------|
+
 	public Emitter getEmitter() {
 		return emitter;
 	}
 
 	public void setEmitter(Emitter emitter) {
+		Emitter oldValue = this.emitter;
 		this.emitter = emitter;
+		firePropertyChange("Emitter", oldValue, emitter);
 	}
 
 	public List<Initializer> getInitializerList() {
 		return initializerList;
 	}
 
-	public void setInitializerList(ArrayList<Initializer> initializerList) {
+	public void setInitializerList(List<Initializer> initializerList) {
+		List<Initializer> oldValue = this.initializerList;
 		this.initializerList = initializerList;
-	}
-
-	public List<Operator> getOperatorList() {
-		return operatorList;
+		firePropertyChange("InitializerList", oldValue, initializerList);
 	}
 
 	public List<Actor> getChildList() {
@@ -75,19 +112,29 @@ public class Actor extends Sprite{
 	}
 
 	public void setChildList(List<Actor> childList) {
+		List<Actor> oldValue = this.childList;
 		this.childList = childList;
+		firePropertyChange("ChildList", oldValue, childList);
+	}
+
+	public List<Operator> getOperatorList() {
+		return operatorList;
 	}
 
 	public void setOperatorList(ArrayList<Operator> operatorList) {
+		List<Operator> oldValue = this.operatorList;
 		this.operatorList = operatorList;
+		firePropertyChange("OperatorList", oldValue, operatorList);
 	}
-	
+
 	public String getActorType() {
 		return actorType;
 	}
 
 	public void setActorType(String actorType) {
+		String oldValue = this.actorType;
 		this.actorType = actorType;
+		firePropertyChange("Type", oldValue, actorType);
 	}
 
 	public String getActorName() {
@@ -95,10 +142,11 @@ public class Actor extends Sprite{
 	}
 
 	public void setActorName(String actorName) {
+		String oldValue = this.actorName;
 		this.actorName = actorName;
+		firePropertyChange("Name", oldValue, actorName);
 	}
-	
-	
+
 	public int getHealth() {
 		return health;
 	}
